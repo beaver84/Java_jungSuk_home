eb01a05795eb91c6b209a23bda55310ff098db2a
lucasbae <junkaiji@gmail.com>
2021-10-12 17:36:49 +0900

    Initial commit

---
+++ src/Exercise6_23.java
@@ -0,0 +1,34 @@
+//메서드명 : max
+//        기 능 : 주어진 int형 배열의 값 중에서 제일 큰 값을 반환한다.
+//        만일 주어진 배열이 null이거나 크기가 0인 경우, -999999를 반환한다.
+//        반환타입 : int
+//        매개변수 : int[] arr - 최대값을 구할 배열
+
+class Exercise6_23{
+    public static int max(int[] arr) {
+        if ((arr == null)){
+            int max_num = -999999;
+        }
+        int max_num = 0;
+        int temp = 0;
+        for (int a : arr) {
+            if(a > max_num){
+                temp = max_num;
+                max_num = a;
+                a = temp;
+            }
+        }
+        if ((arr == null) || (max_num == 0)){
+            max_num = -999999;
+        }
+        return max_num;
+    }
+    public static void main(String[] args)
+    {
+        int[] data = {3,2,9,4,7};
+        System.out.println(java.util.Arrays.toString(data));
+        System.out.println("최대값:"+max(data));
+        System.out.println("최대값:"+max(null));
+        System.out.println("최대값:"+max(new int[]{})); // 크기가 0인 배열
+    }
+}
---
+++ src/Exercise7_5.java
@@ -0,0 +1,33 @@
+//class Product
+//{
+//    int price;  //제품의 가격
+//    int bonusPoint;  //제품구매시 제공하는 보너스 점수
+//
+//    Product(int price) {
+//        this.price = price;
+//        bonusPoint = (int) (price / 10.0);
+//    }
+//
+//    public Product() {
+//
+//    }
+//}
+//
+//class Tv extends Product{
+//    Tv() {
+//        super(1000);
+//    }
+//
+//    public String toString() {
+//        return "Tv";
+//    }
+//}
+//
+//public class Exercise7_5 {
+//    public static void main(String[] args) {
+//        Tv t = new Tv();
+//        Product p = new Tv();
+//        System.out.println(p);
+//        System.out.println(p.bonusPoint);
+//    }
+//}
