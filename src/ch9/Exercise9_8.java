64bdf2ea4c7eed327b0a684dcb4982bdaa2788de
lucasbae <junkaiji@gmail.com>
2021-10-16 22:02:08 +0900

    10/16 contains, round 연습문제 풀이

---
+++ src/ch9/Exercise9_8.java
@@ -0,0 +1,19 @@
+package ch9;
+
+
+class Exercise9_8 {
+
+
+
+    public static void main(String[] args) {
+        System.out.println(round(3.1415,1));
+        System.out.println(round(3.1415,2));
+        System.out.println(round(3.1415,3));
+        System.out.println(round(3.1415,4));
+        System.out.println(round(3.1415,5));
+    }
+
+    private static double round(double d, int n) {
+        return Math.round(d * Math.pow(10, n)) / Math.pow(10, n);
+    }
+}
