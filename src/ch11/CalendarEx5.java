27700eb3fe99f8b2f83012a605605a482e46c242
lucasbae <junkaiji@gmail.com>
2021-10-19 18:58:27 +0900

    (자바의 정석)ch11 TreeMap 예제2

---
+++ src/ch11/CalendarEx5.java
@@ -0,0 +1,21 @@
+package ch11;
+
+import com.sun.xml.internal.ws.streaming.MtomStreamWriter;
+
+import java.util.Calendar;
+
+public class CalendarEx5 {
+    public static void main(String[] args) {
+        Calendar date = Calendar.getInstance();
+
+        date.set(2005, 0, 31);
+        System.out.println(toString(date));
+        date.roll(Calendar.MONTH, 1);
+        System.out.println(toString(date));
+
+    }
+
+    public static String toString(Calendar date) {
+        return date.get(Calendar.YEAR) + "년" + (date.get(Calendar.MONTH) + 1) + "월 " + date.get(Calendar.DATE) + "일";
+    }
+}
