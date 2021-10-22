60aa6cad6567cf170417b75eceb1703532ef7573
lucasbae <junkaiji@gmail.com>
2021-10-22 18:14:49 +0900

    (자바의 정석)ch11 Random 예제4 - batch 데이터 생성

---
+++ src/ch11/RandomEx4.java
@@ -0,0 +1,37 @@
+package ch11;
+
+public class RandomEx4 {
+    final static int RECORD_NUM = 10;
+    final static String TABLE_NAME = "TEST_TABLE";
+    final static String[] CODE1 = {"010", "011", "017", "018", "019"};
+    final static String[] CODE2 = {"남자", "여자"};
+    final static String[] CODE3 = {"10대", "20대", "30대", "40대", "50대"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < RECORD_NUM; i++) {
+            System.out.println("INSERT INTO " + TABLE_NAME
+                                + " VALUES ("
+                                + " '" + getRandArr(CODE1) + "'"
+                                + ", '" + getRandArr(CODE2) + "'"
+                                + ", '" + getRandArr(CODE3) + "'"
+                                + ",  " + getRand(100, 200) //100~200 사이의 값을 얻는다.
+                                + "); ");
+        }
+    }
+
+    public static String getRandArr(String[] arr) {
+        return arr[getRand(arr.length - 1)];
+    }
+
+    public static int getRand(int n) {
+        return getRand(0, n);
+    }
+
+    public static int getRand(int from, int to) {
+        /*
+        10에서 20 사이의 숫자를 생성하려면 다음을 사용할 수 있습니다.
+        int myNumber = new Random().nextInt(11) + 10
+         */
+        return (int) (Math.random() * (Math.abs(to - from) + 1)) + Math.min(from, to);
+    }
+}
